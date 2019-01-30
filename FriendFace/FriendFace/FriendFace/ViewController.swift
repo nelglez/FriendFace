ba3692c88eab5d1e6a0fa4d6aa20d0b9affa3154
Paul Yi <yipaulc@gmail.com>
2019-01-30 00:35:29 -0800

    Initial commit

---
+++ FriendFace/FriendFace/FriendFace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendFace
+//
+//  Created by Paul Yi on 1/30/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
