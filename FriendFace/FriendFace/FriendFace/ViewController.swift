7f409a316ebe12fbcda1d5a00395410f7fe7af96
Paul Yi <yipaulc@gmail.com>
2019-01-30 01:07:38 -0800

    Add networking and search bar to ViewController

--- FriendFace/FriendFace/FriendFace/ViewController.swift
+++ FriendFace/FriendFace/FriendFace/ViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UISearchResultsUpdating {
+    var friends = [Friend]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let search = UISearchController(searchResultsController: nil)
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Find a friend"
+        search.searchResultsUpdater = self
+        navigationItem.searchController = search
+        
+        DispatchQueue.global().async {
+            do {
+                let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+                let data = try Data(contentsOf: url)
+                
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let downloadedFriends = try decoder.decode([Friend].self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.friends = downloadedFriends
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let friend = friends[indexPath.row]
+        
+        cell.textLabel?.text = friend.name
+        cell.detailTextLabel?.text = friend.friends.map { $0.name }.joined(separator: ",")
+        return cell
+    }
+    
+    
 
 
 }
